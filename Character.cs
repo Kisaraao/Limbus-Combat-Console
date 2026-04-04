e5d9401ca286349a1d37ec045676dc8bab170f23
Kisaraao <2161349546@qq.com>
2026-04-04 21:43:06 +0800

    json读取

---
+++ Character.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Limbus_Combat_Console
+{
+    class Character
+    {
+        // 名称
+        public string Name { get; set; }
+        // 等级
+        public int Level { get; set; }
+        // 特性关键词
+        public List<String>? Tags { get; set; } = new List<String>();
+        // 血量
+        public int HealthInitial { get; set; }
+        public (int min, int max) HealthRange { get; set; }
+        // 理智
+        public int SanityInitial { get; set; }
+        public (int min, int max) SanityRange { get; set; }
+        // 防御等级
+        public int DefenseBase { get; set; }
+        public int DefenseVariable { get; set; }
+        // 混乱阈值
+        public Queue<double> Confusions { get; set; } = new Queue<double>();
+        // 速度
+        public (int min, int max) SpeedRange { get; set; }
+        // 攻击属性抗性
+        public Dictionary<string, double>? AttackAttrResist { get; set; } = new Dictionary<string, double>();
+        // 罪孽属性抗性
+        public Dictionary<string, double>? SinAttrResist { get; set; } = new Dictionary<string, double>();
+    
+        public void PrintInfos()
+        {
+            Console.WriteLine("名称：{0}\n等级：{1}", Name, Level);
+            Console.Write("特性关键词：");
+            foreach (var item in Tags)
+            {
+                Console.Write("[{0}]", item);
+            }
+            Console.WriteLine("\n血量：[初始值：{0}][上下限：<{1}>-<{2}>]", HealthInitial, HealthRange.min, HealthRange.max);
+            Console.WriteLine("理智：[初始值：{0}][上下限：<{1}>-<{2}>]", SanityInitial, SanityRange.min, SanityRange.max);
+            Console.WriteLine("速度：[上下限：<{0}>-<{1}>]", SpeedRange.min, SpeedRange.max);
+            Console.WriteLine("防御等级：[基础值：{0}][变动值：{1}]", DefenseBase, DefenseVariable);
+            if (Confusions.Count > 0) { Console.Write("混乱阈值：", Math.Round(Confusions.Peek() * HealthRange.max)); }
+            foreach (var item in Confusions) { Console.Write("[{0}]", item); }
+            //Console.Write("物理属性抗性：");
+            //foreach(var item in AttackAttrResist){ Console.Write("[{0}：x{1}]", item.Key, item.Value); }
+            //Console.Write("\n罪孽属性抗性：");
+            //foreach (var item in SinAttrResist) { Console.Write("[{0}：x{1}]", item.Key, item.Value); }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Reflection.Emit;
+using System.Text.Json.Nodes;
+using System.Xml.Linq;
+
+namespace Limbus_Combat_Console
+{
+
+    public static class Global
+    {
+        public static Dictionary<string, ConsoleColor> SinType { get; set; }
+        public static List<string> AttackType { get; set; }
+    }
+
+    class JsonParser
+    {
+        public void LoadSinType(string path)
+        {
+            Global.SinType = new Dictionary<string, ConsoleColor>();
+            JsonNode root = JsonNode.Parse(File.ReadAllText(path));
+            foreach (var item in root["Sin"] as JsonArray) 
+            {
+                Global.SinType.Add(item["Name"].GetValue<string>(), (ConsoleColor)item["Color"].GetValue<int>());
+            }
+        }
+        
+        public void LoadAttackType(string path)
+        {
+            Global.AttackType = new List<string>();
+            JsonNode root = JsonNode.Parse(File.ReadAllText(path));
+            foreach(var item in root["Attack"] as JsonArray){ Global.AttackType.Add(item.GetValue<string>()); }
+        }
+
+        public Character LoadCharacter(string path)
+        {
+            Character character = new Character();
+            JsonNode root = JsonNode.Parse(File.ReadAllText(path));
+            character.Name = root["Name"].GetValue<string>();
+            character.Level = root["Level"].GetValue<int>();
+            foreach(var item in root["Tags"] as JsonArray)
+            {
+                character.Tags.Add(item.GetValue<string>());
+            }
+            character.HealthRange = (root["Health"][0]["Min"].GetValue<int>(), root["Health"][0]["Max"].GetValue<int>());
+            character.HealthInitial = root["Health"][0]["Initial"].GetValue<int>();
+            character.SanityRange = (root["Sanity"][0]["Min"].GetValue<int>(), root["Sanity"][0]["Max"].GetValue<int>());
+            character.HealthInitial = root["Sanity"][0]["Initial"].GetValue<int>();
+            character.SpeedRange = (root["Speed"][0]["Min"].GetValue<int>(), root["Speed"][0]["Max"].GetValue<int>());
+            character.DefenseBase = root["Defense"][0]["Base"].GetValue<int>();
+            character.DefenseVariable = root["Defense"][0]["Variable"].GetValue<int>();
+            foreach(var item in root["Confusion"] as JsonArray)
+            {
+                character.Confusions.Enqueue(item.GetValue<double>());
+            }
+            return character;
+        }
+    }
+
+    class Program
+    {
+        static public void PrintSinAttributes()
+        {
+            Console.Write("全局罪孽属性：");
+            foreach (var item in Global.SinType)
+            {
+                Console.ForegroundColor = item.Value;
+                Console.Write("[" + item.Key + "]");
+            }
+            Console.Write("\n");
+            Console.ResetColor();
+        }
+
+        static public void PrintAttackAttributes()
+        {
+            Console.Write("全局攻击属性：");
+            foreach (var item in Global.AttackType)
+            {
+                Console.Write("[" + item + "]");
+            }
+            Console.Write("\n");
+        }
+
+        static void Main()
+        {
+            JsonParser parser = new JsonParser();
+            parser.LoadSinType("../../../json/Type.json");
+            parser.LoadAttackType("../../../json/Type.json");
+            Character player = parser.LoadCharacter("../../../json/Callisto.json");
+            
+            PrintSinAttributes();
+            PrintAttackAttributes();
+            player.PrintInfos();
+        }
+    }
+}
